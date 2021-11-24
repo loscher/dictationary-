bacb475ea2ab4daeab70d4a086833c4ac552c7c7
loscher <79922035+loscher@users.noreply.github.com>
2021-11-23 20:56:06 -0500

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,34 @@
+'''
+Created on Nov 18, 2021
+
+@author: loganoscher
+dictionary
+''' 
+def main():
+    file = open("/Users/loganoscher/Library/Mobile Documents/com~apple~TextEdit/Documents/testie.txt", "r")
+    dictionary = dict()
+    for line in file:
+        #line = line.remove('-', ':') 
+        line = line.replace('-', '')
+        line = line.replace('  ', '')
+        line = line.replace('   ', '')
+        line = line.replace(':', '')
+        line = line.replace('.', '')
+        line = line.replace('!', '')
+        line = line.replace(',', '')
+        line = line.replace('?', '')
+        line = line.strip()
+        text = line.split(" ")  
+        for word in text:
+            if word in dictionary:
+                dictionary[word] = dictionary[word] + 1
+            else:
+                dictionary[word] = 1
+    dictionary = sorted(dictionary.items(), key=lambda item: item[1])
+    for x in dictionary:
+        print(x)
+if __name__ == "__main__":
+    main()
+#sorted(d.items(), key=lambda item: item[1])
+#for key in list(dictionary.keys()):
+    #return key, ",", dictionary[key]
